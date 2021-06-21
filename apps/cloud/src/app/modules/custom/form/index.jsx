1194b13c7ef77b6c03d90dc2dbff4b77942d69fd
Mario Octavio <mario@kleeen.software>
2021-06-21 17:35:12 -0600

    Google form

--- apps/cloud/src/app/modules/custom/form/index.jsx
+++ apps/cloud/src/app/modules/custom/form/index.jsx
@@ -5,7 +5,16 @@ import { roleAccessKeyTag } from '@kleeen/common/utils';
 function Form({ translate, ...props }) {
   return (
     <AccessControl id={roleAccessKeyTag(`navigation.form`)}>
-      <div>Hello World from your Custom Module form</div>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdr6PVgqHS163kDcVhP21WsXGEa0RWA1QEa4N1WWOoVqGE2SQ/viewform?embedded=true"
+        width="740"
+        height="1010"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
     </AccessControl>
   );
 }
