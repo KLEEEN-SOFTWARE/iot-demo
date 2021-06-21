cab481ccfdab4a8439a233a4ec38aa0ad968bb13
KLEEEN-SOFTWARE <kleeen-bot@kleeen.software.com>
2021-06-21 22:56:30 +0000

    "commit by kleeen-bot :robot:"

---
+++ apps/cloud/src/app/modules/generated/sites/settings/available-filters.ts
@@ -0,0 +1,7 @@
+export const availableFilters = [
+  { name: 'SiteMap', statisticalType: 'Data - Categorical' },
+  { name: 'SystemHealth', statisticalType: 'Data - Categorical - ordered - Severity Ranking' },
+  { name: 'Sensor', statisticalType: 'Data - Categorical' },
+  { name: 'SiteStatus', statisticalType: 'Data - Categorical - ordered - Severity Ranking' },
+  { name: 'Node', statisticalType: 'Data - Categorical' },
+];
